90a3b71a3c11cd01e021552881d2b81403721b45
muhammmedfahim4321 <fahimmuhammedfahimkp@gmail.com>
2024-02-09 15:04:15 +0530

    static files issues resolved

--- base/settings.py
+++ base/settings.py
@@ -183,8 +183,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles_build'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles_build'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
