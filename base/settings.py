6d427a14b3477a48b5c976b1e8597be95f7a5007
muhammmedfahim4321 <fahimmuhammedfahimkp@gmail.com>
2024-02-09 15:19:57 +0530

    changed to whitenose

--- base/settings.py
+++ base/settings.py
@@ -88,6 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     
     #3rd party middlewares
     
@@ -108,7 +109,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'whitenoise.middleware.WhiteNoiseMiddleware'
+                
             ],
         },
     },
